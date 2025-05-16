05219852f0604562dd8c2c3965f385f47f422b5f
SoyMori <azetrom.121@gmail.com>
2025-05-16 05:46:34 -0700

    final

--- src/main/java/ReportGenerator.java
+++ src/main/java/ReportGenerator.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class ReportGenerator {
     static class TaskRunnable implements Runnable {
@@ -22,22 +25,69 @@ public class ReportGenerator {
 
         @Override
         public void run() {
-            //TODO:
-            // - Read all lines from the input file (path)
-            // - For each line, parse product ID, amount, and discount
-            // - The format of the files are like this:
-            //      [productId],[amount],[discountAmount]
-            // - Find the corresponding product from catalog
-            // - Calculate discounted cost and update total stats (totalAmount, totalCost, totalDiscountSum, totalLines)
-            // - Track the most expensive purchase after discount
+            try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+                String line;
+                int productID;
+                int amount;
+                int discount;
+                double price;
+
+                while((line = reader.readLine()) != null){
+                    int start = 0 , j = 0 ;
+
+                    productID = Integer.parseInt(line.substring(0,1)) ;
+                    productID--;
+                    line = line.substring(2) ;
+
+                    for (  ; line.charAt(j) != ',' ; j++ ) ;
+                    amount = Integer.parseInt(line.substring(start, j));
+                    totalAmount += amount ;
+
+                    start = j + 1;
+                    discount = Integer.parseInt(line.substring(start));
+
+                    price = productCatalog[productID].price;
+
+                    totalDiscountSum += amount*discount;
+                    price *= (double)(100 - discount)/100;
+                    totalCost += price;
+
+                    if ( highestCostAfterDiscount < amount * price ){
+                        highestCostAfterDiscount = amount * price ;
+                        mostExpensiveProduct = productCatalog[productID] ;
+                    }
+                    totalLines++;
+                }
+
+                if ( path.equals("src\\main\\resources\\2021_order_details.txt"))
+                    Thread.sleep(100);
+                else if (path.equals("src\\main\\resources\\2022_order_details.txt"))
+                    Thread.sleep(200);
+                else if (path.equals("src\\main\\resources\\2023_order_details.txt"))
+                    Thread.sleep(300);
+                else
+                    Thread.sleep(400);
+
+                makeReport();
+
+            } catch (IOException | InterruptedException e) {
+                System.err.println("Error reading file" + e.getMessage());
+            }
         }
 
         public void makeReport() {
-            // TODO:
-            // - Print the filename
-            // - Print total cost and total items bought
-            // - Calculate and print average discount
-            // - Display info about the most expensive purchase after discount
+            DecimalFormat df = new DecimalFormat("#.##");
+
+            System.out.println("The work related to this file has been completed.\n" + path + "\n");
+            System.out.println("total cost : " + df.format(totalCost) );
+            System.out.println("total lines read : " + totalLines );
+            System.out.println("total items bought : " + totalAmount );
+            System.out.println("average discount : " + df.format((double)(totalDiscountSum) / totalAmount) );
+            System.out.println("most expensive purchase after discount : " + df.format(highestCostAfterDiscount));
+            System.out.println("details of the most expensive product : " + mostExpensiveProduct);
+            System.out.println();
+            System.out.println();
+            System.out.println();
         }
     }
 
@@ -52,38 +102,58 @@ public class ReportGenerator {
             this.price = price;
         }
 
-        public int getProductID() {
-            return productID;
-        }
+        public int getProductID() { return productID; }
 
-        public String getProductName() {
-            return productName;
-        }
+        public String getProductName() { return productName; }
 
-        public double getPrice() {
-            return price;
+        public double getPrice() {return price;}
+
+        @Override
+        public String toString() {
+            return productID + " " + productName + " " + price ;
         }
     }
-    private static final String[] ORDER_FILES = {
-            // TODO: Define the paths to the order detail text files in the resources folder
-    };
-
-    static Product[] productCatalog = new Product[10];
+    static Product[] productCatalog = new Product[9];
 
     public static void loadProducts() throws IOException {
-        // TODO:
-        // - Read lines from Products.txt
-        // - For each line, parse product ID, name, and price
-        // - The format of the file is like this:
-        //      [productId],[name],[price]
-        // - Store Product objects in the productCatalog array
+        String path = "src\\main\\resources\\Products.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            int productID;
+            String productName;
+            double price;
+            for (int i=0 ; (line = reader.readLine()) != null ; i++) {
+                int start = 0 , j = 0 ;
+                productID = Integer.parseInt(line.substring(0,1)) ;
+                line = line.substring(2) ;
+                for (  ; line.charAt(j) != ',' ; j++ ) ;
+                productName = (line.substring(start, j));
+
+                start = j + 1;
+                price = Double.parseDouble(line.substring(start));
+
+                productCatalog[i] = new Product(productID, productName, price);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file" + e.getMessage());
+        }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        // TODO:
-        // - Create one TaskRunnable and Thread for each order file
-        // - Start all threads
-        // - Wait for all threads to finish
-        // - After all threads are done, call makeReport() on each TaskRunnable
+    public static void main(String[] args) {
+        try {
+            loadProducts();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Thread t1 = new Thread(new TaskRunnable("src\\main\\resources\\2021_order_details.txt"));
+        Thread t2 = new Thread(new TaskRunnable("src\\main\\resources\\2022_order_details.txt"));
+        Thread t3 = new Thread(new TaskRunnable("src\\main\\resources\\2023_order_details.txt"));
+        Thread t4 = new Thread(new TaskRunnable("src\\main\\resources\\2024_order_details.txt"));
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
 }
--- src/main/java/TypingTest.java
+++ src/main/java/TypingTest.java
@@ -1,63 +1,94 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
 
 public class TypingTest {
 
-    private static String lastInput = "";
-    private static Scanner scanner = new Scanner(System.in);
-    public static class InputRunnable implements Runnable {
+    private static final Scanner scanner = new Scanner(System.in);
 
-        //TODO: Implement a thread to get user input without blocking the main thread
-        @Override
-        public void run() {
-
-        }
-    }
-
-
-    public static void testWord(String wordToTest) {
+    public static int testWord(String wordToTest) {
         try {
+            int result = 0 ;
             System.out.println(wordToTest);
-            lastInput = "";
+            String lastInput;
 
-            // TODO
+            lastInput =scanner.nextLine();
 
             System.out.println();
             System.out.println("You typed: " + lastInput);
             if (lastInput.equals(wordToTest)) {
                 System.out.println("Correct");
+                result++;
             } else {
                 System.out.println("Incorrect");
             }
-
+            return result ;
         } catch (Exception e) {
             e.printStackTrace();
+            return -1 ;
         }
     }
 
     public static void typingTest(List<String> inputList) throws InterruptedException {
-
-        for (int i = 0; i < inputList.size(); i++) {
-            String wordToTest = inputList.get(i);
-            testWord(wordToTest);
+        int correctNum = 0 ;
+        for (String wordToTest : inputList) {
+            correctNum += testWord(wordToTest);
             Thread.sleep(2000); // Pause briefly before showing the next word
         }
 
-        // TODO: Display a summary of test results
+        System.out.println("You answered " + correctNum + " test correctly and " + (10-correctNum) +" test incorrectly.");
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static int[] generateRandomNumbers(int count, int min, int max) {
+        if (count <= 0 || min >= max) {
+            throw new IllegalArgumentException("The input is invalid.");
+        }
+
+        Random random = new Random();
+        int[] numbers = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            numbers[i] = random.nextInt(max - min + 1) + min;
+        }
+
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        List<String> listWords = new ArrayList<>();
         List<String> words = new ArrayList<>();
-        words.add("remember");
-        words.add("my friend");
-        words.add("boredom");
-        words.add("is a");
-        words.add("crime");
+        listWords.add("remember");
+        listWords.add("my friend");
+        listWords.add("boredom");
+        listWords.add("is a");
+        listWords.add("crime");
 
-        // TODO: Replace the hardcoded word list with words read from the given file in the resources folder (Words.txt)
-        typingTest(words);
+        String path = "src\\main\\resources\\Words.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                listWords.add(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file" + e.getMessage());
+        }
 
-        System.out.println("Press enter to exit.");
+        int[] randomNumbers = generateRandomNumbers(10, 0, 104);
+        for ( int i = 0 ; i < 10 ; i++)
+            words.add(listWords.get(randomNumbers[i]));
+        Thread thread = new Thread(() -> {
+            try {
+                typingTest(words);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Press enter to exit.");
+        });
+
+        thread.start();
     }
 }
